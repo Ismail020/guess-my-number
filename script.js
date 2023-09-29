5b94ede99baa6bcae65964ce247ad7e18565ea3e
Ismail020 <isocetin1907@gmail.com>
2023-09-29 14:09:41 +0200

    Game functionality

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
 'use strict';
+
+const getSecretNumber = function () {
+  return Math.trunc(Math.random() * 20) + 1;
+};
+
+let secretNumber = getSecretNumber();
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const bodyBackgroundColor = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔️ No Number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    displayNumber(secretNumber);
+
+    bodyBackgroundColor('#60b347');
+    document.querySelector('.number').style.width = '30rem';
+
+    // Highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('💥 You lost the game!');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = getSecretNumber();
+
+  displayMessage('Start guessing...');
+  displayScore(score);
+  displayNumber('?');
+
+  document.querySelector('.guess').value = '';
+
+  bodyBackgroundColor('#222');
+  document.querySelector('.number').style.width = '15rem';
+});
